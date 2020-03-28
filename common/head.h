e2852591794ab24b1508f6b8b5135360ab8f5b49
Ginakira <ginakira@foxmail.com>
2020-03-28 15:23:17 +0800

    v0.2

--- common/head.h
+++ common/head.h
@@ -8,8 +8,10 @@
 #ifndef HEAD_H
 #define HEAD_H
 #include <arpa/inet.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
