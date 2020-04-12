eddffbcbcdb7b705140221634ac7991798e571d9
Ginakira <ginakira@foxmail.com>
2020-04-12 14:58:06 +0800

    select_iom

--- server/backup/server_select_io_multiplexing.c
+++ server/backup/server_select_io_multiplexing.c
@@ -1,9 +1,9 @@
 /************************************************************
-    File Name : server_io_multiplexing.c
+    File Name : server_select_iom.c
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/04/11 11:36:04
+    Created Time: 2020/04/12 14:27:31
 ************************************************************/
 
 #include "../common/color.h"
@@ -11,78 +11,95 @@
 #include "../common/head.h"
 #include "../common/tcp_server.h"
 
-#define MAX_CLIENTS 128
+#define MAX_CLIENTS 256
+#define BUFF_SIZE 512
 
-int socket_fds[MAX_CLIENTS];
-int client_connected = 0;
-int max_fd_number = 0x80000000;
+int sock_fds[MAX_CLIENTS];
+int max_fd_number;
+
+char ch_char(char c) {
+    if (c >= 'a' && c <= 'z')
+        return c - 32;
+    else
+        return c;
+}
 
 int main(int argc, char **argv) {
-    int server_listen, port;
-
     if (argc != 2) {
         fprintf(stderr, "Usage: %s [port]\n", argv[0]);
         exit(1);
     }
 
-    port = atoi(argv[1]);
-
-    if ((server_listen = socket_create(port)) < 0) {
+    int server_listen, fd;
+    if ((server_listen = socket_create(atoi(argv[1]))) < 0) {
         perror("socket_create");
         exit(1);
     }
 
-    fd_set watching_fds;
-    struct timeval tv;
-    tv.tv_sec = 20;
-    tv.tv_usec = 0;
-    char buff[1024] = {0};
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        sock_fds[i] = -1;
+    }
+
+    fd_set event_set;
+    sock_fds[0] = server_listen;
     max_fd_number = server_listen;
 
     while (1) {
-        FD_ZERO(&watching_fds);
-        FD_SET(server_listen, &watching_fds);
+        FD_ZERO(&event_set);
         for (int i = 0; i < MAX_CLIENTS; ++i) {
-            if (socket_fds[i] != 0) {
-                FD_SET(socket_fds[i], &watching_fds);
-                max_fd_number = socket_fds[i] > max_fd_number ? socket_fds[i]
-                                                              : max_fd_number;
-            }
+            if (sock_fds[i] < 0) continue;
+            FD_SET(sock_fds[i], &event_set);
+            max_fd_number =
+                sock_fds[i] > max_fd_number ? sock_fds[i] : max_fd_number;
         }
 
-        int retval = select(max_fd_number + 1, &watching_fds, NULL, NULL, &tv);
-
-        if (retval < 0) {
+        int retval;
+        if ((retval = select(max_fd_number + 1, &event_set, NULL, NULL, NULL)) <
+            0) {
             perror("select");
-            break;
+            return 1;
         }
 
-        if (retval == 0) {
-            printf(YELLOW " [TIMEOUT] " NONE "No client is active.\n");
-            continue;
-        }
-
-        if (FD_ISSET(server_listen, &watching_fds)) {
-            int new_fd = accept(server_listen, NULL, NULL);
-            if (new_fd < 0) {
+        if (FD_ISSET(sock_fds[0], &event_set)) {
+            if ((fd = accept(server_listen, NULL, NULL)) < 0) {
                 perror("accept");
-                break;
+                continue;
             }
-            socket_fds[client_connected++] = new_fd;
-            printf(GREEN " [LOGIN] " NONE "New client connected!\n");
-            if (--retval == 0) continue;
+            retval--;
+
+            int ind;
+            for (ind = 1; ind < MAX_CLIENTS; ++ind) {
+                if (sock_fds[ind] < 0) {
+                    sock_fds[ind] = fd;
+                    printf(GREEN "[Login]" NONE " New sock_fd %d logged in\n",
+                           sock_fds[ind]);
+                    break;
+                }
+            }
+            if (ind == MAX_CLIENTS) {
+                printf(YELLOW "[FULL]" NONE " Too many connections!\n");
+                close(fd);
+            }
         }
 
-        for (int i = 0; i < client_connected; ++i) {
-            if (socket_fds[i] && FD_ISSET(socket_fds[i], &watching_fds)) {
-                memset(buff, 0, sizeof(buff));
-                if (recv(socket_fds[i], buff, sizeof(buff), 0) <= 0) {
-                    socket_fds[i] = 0;
-                    continue;
+        for (int i = 1; retval && i < MAX_CLIENTS; ++i) {
+            if (sock_fds[i] < 0) continue;
+            if (FD_ISSET(sock_fds[i], &event_set)) {
+                retval--;
+                char buff[BUFF_SIZE] = {0};
+                if (recv(sock_fds[i], buff, BUFF_SIZE, 0) > 0) {
+                    printf(GREEN "[Recv]" NONE " %s\n", buff);
+                    for (int i = 0; i < strlen(buff); ++i) {
+                        buff[i] = ch_char(buff[i]);
+                    }
+                    send(sock_fds[i], buff, strlen(buff), 0);
+                } else {
+                    close(sock_fds[i]);
+                    sock_fds[i] = -1;
                 }
-                printf(GREEN "[%d]" NONE " : %s\n", socket_fds[i], buff);
             }
         }
     }
+
     return 0;
 }
